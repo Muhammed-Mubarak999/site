b05d75f11a41d9d5d30e47a5d6b7120bc2b1f41e
Muhammed Mubarak <127791160+Muhammed-Mubarak999@users.noreply.github.com>
2023-04-23 15:17:51 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,15 @@
 // Initialize Firebase
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import { firebaseConfig } from './firebaseConfig.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyBsS-I3SDok19oYEPv4wYsixYdrwyH-lrI",
+  authDomain: "mywebsite-5bd3e.firebaseapp.com",
+  databaseURL: "https://mywebsite-5bd3e-default-rtdb.firebaseio.com",
+  projectId: "mywebsite-5bd3e",
+  storageBucket: "mywebsite-5bd3e.appspot.com",
+  messagingSenderId: "1003118523115",
+  appId: "1:1003118523115:web:858da7a075e54ba62f44ae",
+  measurementId: "G-9L374M2C46"
+};
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
