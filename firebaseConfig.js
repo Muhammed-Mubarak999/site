3b7386c7b07d6cd9e2ff59c6bd41c4a310895eca
Muhammed Mubarak <127791160+Muhammed-Mubarak999@users.noreply.github.com>
2023-04-23 15:07:56 +0200

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsS-I3SDok19oYEPv4wYsixYdrwyH-lrI",
+  authDomain: "mywebsite-5bd3e.firebaseapp.com",
+  databaseURL: "https://mywebsite-5bd3e-default-rtdb.firebaseio.com",
+  projectId: "mywebsite-5bd3e",
+  storageBucket: "mywebsite-5bd3e.appspot.com",
+  messagingSenderId: "1003118523115",
+  appId: "1:1003118523115:web:858da7a075e54ba62f44ae",
+  measurementId: "G-9L374M2C46"
+};
