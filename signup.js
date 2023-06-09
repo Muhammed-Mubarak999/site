adfd18e2054c1e3201aaf6cc1de5cd16375c2c92
Muhammed Mubarak <127791160+Muhammed-Mubarak999@users.noreply.github.com>
2023-06-09 20:16:43 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,12 +1,12 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyBsS-I3SDok19oYEPv4wYsixYdrwyH-lrI",
-  authDomain: "mywebsite-5bd3e.firebaseapp.com",
-  databaseURL: "https://mywebsite-5bd3e-default-rtdb.firebaseio.com",
-  projectId: "mywebsite-5bd3e",
-  storageBucket: "mywebsite-5bd3e.appspot.com",
-  messagingSenderId: "1003118523115",
-  appId: "1:1003118523115:web:858da7a075e54ba62f44ae",
-  measurementId: "G-9L374M2C46"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCC1KWnW5P6NSLelJTrxb9TG8srodutO9Y",
+  authDomain: "secirityx.firebaseapp.com",
+  projectId: "secirityx",
+  storageBucket: "secirityx.appspot.com",
+  messagingSenderId: "215784175374",
+  appId: "1:215784175374:web:236450bbc2e0c7d50a495f",
+  measurementId: "G-MJRZHJK8P7"
 };
 firebase.initializeApp(firebaseConfig);
 // Firebase authentication
